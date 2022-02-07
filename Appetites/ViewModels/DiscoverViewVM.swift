9ea67b4fa1e8ec252aa3b4d297b21b65536913c2
Chavin2543 <rungallday@hotmail.com>
2022-02-07 22:23:32 +0700

    remote push test

---
+++ Appetites/ViewModels/DiscoverViewVM.swift
@@ -0,0 +1,26 @@
+//
+//  DiscoverViewVM.swift
+//  Appetites
+//
+//  Created by chavin Panicharoen on 30/1/2565 BE.
+//
+
+import Foundation
+
+
+
+class DiscoverViewVM: ObservableObject {
+    @Published var isSearcing:Bool = false
+    
+    
+    init() {
+    }
+    
+    func search() {
+        isSearcing = true
+    }
+    
+    deinit{
+    }
+}
+
