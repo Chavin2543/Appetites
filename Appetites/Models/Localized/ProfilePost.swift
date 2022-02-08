f893397b8168b857e9c2589a1eb70b26d1b80848
Armi4d <arm.agt26@gmail.com>
2022-02-08 21:58:34 +0700

    Fix Merging Issues

---
+++ Appetites/Models/Localized/ProfilePost.swift
@@ -0,0 +1,34 @@
+//
+//  ProfilePost.swift
+//  Appetites
+//
+//  Created by chavin Panicharoen on 8/2/2565 BE.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ProfilePost: View {
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                WebImage(url: URL(string: "https://images.pexels.com/photos/2097090/pexels-photo-2097090.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"))
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: geometry.size.width, height: 232, alignment: .center)
+                .clipped()
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vulputate eget eu vitae euismod amet. Ultrices enim vivamus sem rhoncus, facilisis amet risus,")
+                    .foregroundColor(.white)
+                    .frame(width: geometry.size.width-64, height: 120, alignment: .center)
+                    .lineLimit(4)
+            }
+            
+        }
+    }
+}
+
+struct ProfilePost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfilePost()
+    }
+}
